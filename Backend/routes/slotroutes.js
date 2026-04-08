20d58eee218282fb5229a3b4c19b43a428fac514
Nilesh Prajapati <nileshprajapati1108@gmail.com>
2026-04-08 17:58:55 +0530

    Delete Backend directory

--- Backend/routes/slotroutes.js
+++
@@ -1,6 +0,0 @@
-import express from "express";
-import { getSlots } from "../controllers/slotcontroller.js";
-const router = express.Router();
-
-router.get("/:doctorId/:date", getSlots);
-export default router;
--- Backend/routes/statsRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getPublicStats } from "../controllers/statsController.js";
-
-const router = express.Router();
-
-// Public stats used by frontend homepage
-router.get("/", getPublicStats);
-
-export default router;
